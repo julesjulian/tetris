e8e59298737c4d5edc0aed507db57bb1c0357911
Julian Kuhlmann <jlnkuhlmann@gmail.com>
2016-07-03 11:43:58 +0200

    Implemented Piece class.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,19 @@
-from tetris.main import Board
+from tetris.main import Board, Piece
 
 
-def test_initialization():
+def test_board_initialization():
     Board()
 
 
 def test_board_can_be_drawn():
     board = Board()
     board.draw()
+
+
+def test_piece_initialization():
+    Piece()
+
+
+def test_piece_can_be_drawn():
+    piece = Piece()
+    piece.draw()
--- tetris/main.py
+++ tetris/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import random
 
 
 MATTER = '*'
@@ -13,6 +14,7 @@ class Board():
     The board has a fixed height and width of 20 grid boxes, and a frame around it is displayed on
     all sides except for the top, where pieces can enter.
     """
+
     _WIDTH = 20
     _HEIGHT = 20
 
@@ -20,6 +22,31 @@ class Board():
         for line in range(self._HEIGHT):
             print(MATTER + VOID * self._WIDTH + MATTER)
         print(MATTER * (self._WIDTH + 2))
+
+
+class Piece():
+    """
+    A Tetris piece.
+
+    The piece can have one of five shapes.
+    """
+
+    _SHAPES = (
+        ((True,), (True,), (True,), (True,)), # Stick
+        ((True, False), (True, False), (True, True)), # L
+        ((False, True), (False, True), (True, True)), # Inverse L
+        ((False, True), (True, True), (True, False)), # Inverse S
+        ((True, True), (True, True)) # Block
+    )
+
+    def __init__(self):
+        self._shape = self._SHAPES[random.randint(0, len(self._SHAPES) - 1)]
+
+    def draw(self):
+        for line in self._shape:
+            for column in line:
+                print(MATTER if column else VOID, end='')
+            print('')
 
 
 def _initialize_game():
